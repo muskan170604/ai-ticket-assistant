967958ffabfd6d4740055c43a5bbd549e1490006
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-06-08 14:15:49 +0530

    routes done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
+import userRoutes from "./routes/user.js"
 
 const PORT=process.env.PORT || 3000
 const app=express()
@@ -8,6 +9,8 @@ const app=express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/api/auth",userRoutes)
 
 
 mongoose
